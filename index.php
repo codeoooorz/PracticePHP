5d9f73e5e07453920309b97430f1f8fc41452160
codeoooorz <cp3joa87@hotmail.com>
2019-03-23 16:21:44 +0800

    Add FIle

---
+++ index.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+<body>
+<p>
+    <?php 
+
+        // $arr = getdate(); // 獲取當下時間參數
+        // foreach($arr as $index => $value) {
+        //     echo nl2br("$index => $value\n");
+        // }
+
+        // $arr = localtime(time(), 0); // 獲取當下時間參數
+        // foreach($arr as $index => $value) {
+        //     echo nl2br("$index => $value\n");
+        // }
+        // printf("=================<br>");
+        // $arr = localtime(time(), 1); // 獲取當下時間參數
+        // foreach($arr as $index => $value) {
+        //     echo nl2br("$index => $value\n");
+        // }
+
+        
+
+
+
+
+      ?>
+
+</p>
+</body>
+</html>
